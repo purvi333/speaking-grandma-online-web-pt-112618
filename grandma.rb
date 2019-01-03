7efff1b16c3b0fbc314c1d4c40f4b638468f1664
benefic-search-4949 <purvivaja14@gmail.com>
2019-01-03 02:50:30 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
   print "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "I LOVE YOU GRANDMA"
-  print "I LOVE YOU TOO PUMPKIN"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  print "I LOVE YOU TOO PUMPKIN!"
 else 
   print "NO, NOT SINCE 1938!"
 end
